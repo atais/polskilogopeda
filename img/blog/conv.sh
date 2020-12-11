14883b417e7896d55d294d0ed34f000551d96470
atais <atais.jr@gmail.com>
2020-12-11 18:43:10 +0100

    fix fix fix

--- img/blog/conv.sh
+++ img/blog/conv.sh
@@ -6,6 +6,6 @@ filename="${filename%.*}"
 
 mkdir -p $filename
 
-convert $1 -resize 1140x "$filename/1140.$extension"
-convert $1 -resize 280x "$filename/280.$extension"
+convert $1 -resize 1140x "$filename/1140.png"
+convert $1 -resize 480x "$filename/480.png"
 mv $1 "$filename/orig.$extension"
